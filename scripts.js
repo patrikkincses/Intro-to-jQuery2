b3c2539267669e05e08845f7ca7ceb12f25830df
patrikkincses <patrikkincses@users.noreply.github.com>
2021-01-25 16:13:36 +0100

    Intro to jQuery2

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let color = ('purple');
+
+let word = ('cool');
+
+let number = 10;
+
+if (number > 100) {
+    $('.East').text("whoah, its a big number");
+}  else {
+    $('.East').text("just a regular number, please.");
+}
+
+if (color = 'purple') {
+    $('.North').css('background', 'purple');
+}
+
+if (word = 'cool') {
+    $('.South').text("Power of DOM");
+}  else {
+    $('.West').text("Power of DOM");
+}
